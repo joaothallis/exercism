27ab3f4cb0eebc27c02105e978b92922ab7a8212
João Thallis <joaothallis@icloud.com>
2023-05-15 00:14:39 -0300

    Complete C hamming

--- c/hamming/hamming.c
+++ c/hamming/hamming.c
@@ -1,1 +1,18 @@
 #include "hamming.h"
+#include <string.h>
+
+int compute(const char *lhs, const char *rhs) {
+  if (strlen(lhs) == strlen(rhs)) {
+    int hamming_distance = 0;
+    int i;
+    for (i = 0; i <= (int)strlen(lhs) - 1; i++) {
+      if (lhs[i] != rhs[i]) {
+        hamming_distance++;
+      }
+    }
+    return hamming_distance;
+
+  } else {
+    return -1;
+  }
+}
